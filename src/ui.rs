6889a2bfbfd42ac9400dff3a9d6c3f4dae1a0ff0
Timo Borer <timoborer@gmail.com>
2024-07-22 19:45:45 +0200

    update ui style

--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,10 @@
-use dialoguer::Select;
+use dialoguer::{theme::ColorfulTheme, Select};
 
 pub fn select(title: &str, options: Vec<&str>) -> String {
-    let selection = Select::new()
+    let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt(title)
         .items(&options)
+        .default(0)
         .interact()
         .unwrap();
     options[selection].to_string()
