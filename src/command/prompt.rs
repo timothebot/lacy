234403c4433858618c558d15f0a8dea9b488a918
Timo Borer <timoborer@gmail.com>
2024-07-23 07:02:01 +0200

    update scoring algorithm, sort select and show score in select

--- src/command/prompt.rs
+++ src/command/prompt.rs
@@ -11,11 +11,24 @@ fn lazy_path_matching(dir: &str, part: &str, real_path: &bool) -> i32 {
     let dir = dir.split("/").last().unwrap_or("");
     if *real_path {
         if dir.to_lowercase().starts_with(part.to_lowercase().as_str()) {
-            score += 2;
+            score += 20;
         }
     }
     if dir.to_lowercase().contains(part.to_lowercase().as_str()) {
-        score += 1;
+        score += 10;
+    }
+    let mut dir_name_mut = dir.to_string();
+    for c in part.chars() {
+        if dir_name_mut.to_lowercase().contains(c.to_ascii_lowercase()) {
+            score += 1;
+            // strip the char to avoid multiple matches
+            dir_name_mut = dir_name_mut.replacen(c, "", 1);
+        } else {
+            score -= 5;
+        }
+    }
+    if dir.to_lowercase() == part.to_lowercase() {
+        score += 50;
     }
     score
 }
@@ -26,7 +39,7 @@ fn resolve_path_part(
     part: &String,
     current_path: PathBuf,
     real_path: bool,
-) -> Result<PathBuf, Option<Vec<PathBuf>>> {
+) -> Result<PathBuf, Option<Vec<(PathBuf, i32)>>> {
     let dirs_res = fs::read_dir(&current_path);
     let Ok(dirs) = dirs_res else {
         return Result::Err(None);
@@ -60,40 +73,44 @@ fn resolve_path_part(
         })
         .collect();
 
-    if scored_dirs.len() == 1 {
-        return Result::Ok(scored_dirs.keys().next().unwrap().clone());
-    }
     if possible_dirs.is_empty() {
         // TODO: search subdirectories
         return Result::Err(None);
     }
+    if scored_dirs.len() == 1 {
+        return Result::Ok(scored_dirs.keys().next().unwrap().clone());
+    }
 
     // everything below the average score is discarded
     let average_score: f32 = scored_dirs.values().sum::<i32>() as f32 / scored_dirs.len() as f32;
-
-    // TODO: sort by score
-    let paths: Vec<PathBuf> = scored_dirs
+    // sort by score
+    let mut paths: Vec<(PathBuf, i32)> = scored_dirs
         .iter()
         .filter(|(_, score)| **score as f32 >= average_score)
-        .map(|dir| {
+        .map(|(dir, score)| {
             let mut new_path = current_path.clone();
-            new_path.push(dir.0);
-            new_path
+            new_path.push(dir);
+            (new_path, *score)
         })
         .collect();
 
+    if paths.len() == 1 {
+        return Result::Ok(paths[0].0.clone());
+    }
+
+    paths.sort_by(|a, b| b.1.cmp(&a.1));
     Result::Err(Some(paths))
 }
 
 /// select one of the given paths
-fn choose_path(possible_paths: Vec<PathBuf>) -> PathBuf {
-    let possible_paths_str: Vec<&str> = possible_paths
+fn choose_path(possible_paths: Vec<(PathBuf, i32)>) -> PathBuf {
+    let possible_paths_str: Vec<String> = possible_paths
         .iter()
-        .map(|path| path.to_str().unwrap())
+        .map(|path| format!("{}: {}", path.1, path.0.to_str().unwrap()))
         .collect();
     PathBuf::from(ui::select(
         "Multiple possibilities found",
-        possible_paths_str,
+        possible_paths_str.iter().map(|s| s.as_str()).collect(),
     ))
 }
 
