2d6c7664c28d0cd1589e36a5c9b997761a043108
Timo Borer <timoborer@gmail.com>
2025-09-06 13:07:15 +0200

    feat: Add shell config for fish and bash

--- src/init.rs
+++ src/init.rs
@@ -11,7 +11,6 @@ function y {{
         echo "Error: No matching directory found for '$*'"
     fi
 }}
-# Auto-completion for y function
 function _y {{
     local dirs
     args="${{words[@]:1}}"
@@ -19,8 +18,63 @@ function _y {{
     dirs=(${{(s: :)dirs}})
     compadd $dirs
 }}
+_lacy() {{
+    compadd prompt complete init help
+}}
+compdef _lacy lacy
 compdef _y y"#
             );
+        }
+        "bash" => {
+            println!(
+                r#"
+y() {{
+    new_path=$(lacy prompt -- "$*")
+    if [ -d "$new_path" ]; then
+        cd "$new_path" || return
+    else
+        echo "Error: No matching directory found for '$*'"
+    fi
+}}
+_y() {{
+    local cur dirs basenames
+    cur="${{COMP_WORDS[*]:1}}"
+    dirs=$(lacy complete -- "$cur")
+    basenames=$(printf '%s\n' $dirs | xargs -n1 basename)
+    COMPREPLY=($(compgen -W "$basenames" -- "$cur"))
+}}
+_lacy() {{
+    local cur
+    cur="${{COMP_WORDS[COMP_CWORD]}}"
+    COMPREPLY=($(compgen -W "prompt complete init help" -- "$cur"))
+}}
+complete -F _lacy -o default -o nospace lacy
+complete -F _y y"#
+            )
+        }
+        "fish" => {
+            println!(r#"
+function y
+    set new_path (lacy prompt -- "$argv")
+    if test -d "$new_path"
+        cd "$new_path"
+    else
+        echo "Error: No matching directory found for '$argv'"
+    end
+end
+function __y_autocomplete
+    set args $argv
+    if test "$args" = ""
+        ls -D --icons=never -1
+    else
+        set dirs (string split ' ' (lacy complete -- "$args"))
+        for dir in $dirs
+            basename $dir
+        end
+    end
+end
+complete --no-files lacy -x -a "prompt complete init help"
+complete --no-files y -r -a "(__y_autocomplete)""#)
         }
         _ => {
             eprintln!("Error: Unsupported shell '{}'", shell);
