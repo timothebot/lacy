9b5d8cb2d7f1f968a27104456ef70a6b2a0b96f6
Linus Tan <linustan@users.noreply.github.com>
2025-09-09 15:03:03 +0900

    feat: Allow users to back out of selection (#27)
    When the selection dialog is opened, the user
    may now back out of it with with q / esc

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,14 @@
 use dialoguer::{theme::ColorfulTheme, Select};
 
-pub fn select(title: &str, options: Vec<String>) -> String {
-    let selection = Select::with_theme(&ColorfulTheme::default())
+pub fn select(title: &str, options: Vec<String>) -> Option<String> {
+    if let Some(selection) = Select::with_theme(&ColorfulTheme::default())
         .with_prompt(title)
         .items(&options)
         .default(0)
-        .interact()
-        .unwrap();
-    options[selection].to_string()
+        .interact_opt()
+        .unwrap()
+    {
+        return Some(options[selection].to_string());
+    }
+    None
 }
