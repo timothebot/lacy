045c547d37a185de7544e0258f52e1240be8fb93
Timo Borer <timoborer@gmail.com>
2025-09-12 18:00:16 +0200

    fix: Update devtools shell to use debug version

--- devtools/shell.sh
+++ devtools/shell.sh
@@ -1,40 +1,5 @@
-# source this with zsh to use dev/release build
+# source this with zsh to use dev build
 
-function yr {
-    new_path=$(../target/release/lacy prompt -- "$*")
-    if [ "$new_path" = "~" ]; then
-        cd ~
-    elif [ -d "$new_path" ]; then
-        cd "$new_path"
-    else
-        echo "Error: No matching directory found for '$*'"
-    fi
-}
-function _yr {
-    local dirs
-    args="${{words[@]:1}}"
-    dirs=$(../target/release/lacy complete -- "$args")
-    dirs=(${{(s: :)dirs}})
-    compadd $dirs
-}
-compdef _yr yr
+alias lacy="$(pwd)/$(dirname "$0")/../target/debug/lacy"
 
-
-function yd {
-    new_path=$(../target/debug/lacy prompt -- "$*")
-    if [ "$new_path" = "~" ]; then
-        cd ~
-    elif [ -d "$new_path" ]; then
-        cd "$new_path"
-    else
-        echo "Error: No matching directory found for '$*'"
-    fi
-}
-function _yd {
-    local dirs
-    args="${{words[@]:1}}"
-    dirs=$(../target/debug/lacy complete -- "$args")
-    dirs=(${{(s: :)dirs}})
-    compadd $dirs
-}
-compdef _yd yd
+eval "$(lacy init zsh)"
